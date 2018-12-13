c6ec3859c32803bea49edb7765006b803fff198b
Ruben Bousbib <rbousbib@gmail.com>
2018-12-13 23:46:47 +0100

    Basic functions to import raw data.

--- data/data_preprocess.py
+++ data/data_preprocess.py
@@ -1,69 +1,51 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import cv2
-import os
-import h5py
+import glob
+import re
 
-def img_to_vect(img_path):
-    img = cv2.imread(img_path,0)
-    img_flatten = np.reshape(img,img.shape[0]*img.shape[1],1)
-    return img_flatten
+root = "data/words/*/*/*.png"
+filenames = sorted(glob.glob(root))
 
-def get_max_shape():
-    path = os.path.dirname(os.path.abspath("__file__")) + '/words'
-    authors = os.listdir(path)
-    max_width,max_height = 0,0
-    for author in authors:
-        path_author = path + '/' + author
-        author_letters = os.listdir(path_author)
-        for letter in author_letters:
-            path_letter = path_author + '/' + letter
-            words_img = os.listdir(path_letter)
-            for img_path in words_img:
-                img = cv2.imread(path_letter + '/' + img_path,0)
-                try:
-                    height,width = img.shape
-                    if height > max_height:
-                        max_height = height
-                    if width > max_width:
-                        max_width = width
-                except:
-                    continue
-    return max_height, max_width
+def get_images():
+    global filenames
+    X = []
+    for file in filenames:
+        img = cv2.imread(file,0)
+        X.append(img)
+    return X
 
-def create_dataset():
-    path = os.path.dirname(os.path.realpath(__file__)) + '/words'
-    nb_letters = 0
-    authors = os.listdir(path)
-    dataset = []
-    for author in authors:
-        path_author = path + '/' + author
-        author_letters = os.listdir(path_author)
-        nb_letters += len(author_letters)
-        for letter in author_letters:
-            path_letter = path_author + '/' + letter
-            words_img = os.listdir(path_letter)
-            try :
-                text = [img_to_vect(path_letter + '/' + img_path) for img_path in words_img]
-                dataset.append(text)
-            except:
-                print(path_letter)
-    print("The dataset includes " + str(nb_letters) + " letters.")
-    return dataset
+def get_words():
+    pattern = "[^ ]+$"
+    words =[]
+    with open("data/words.txt", 'rb') as labels:
+       for line in labels.read().splitlines()[18:]:
+            match = re.findall(pattern,line.decode())[0]
+            words.append(match)
+    return words
 
-# dataset = create_dataset()
-#
-# max_len_seq = 0
-# max_len_img = 0
-# m = len(dataset)
-# for list in dataset:
-#     if len(list) > max_len:
-#         max_len = len(list)
-#     for img in list:
-#         if len(img)>max_len_img:
-#             max_len_img = len(img)
-#
-# X = np.zeros(m,max_len,max_len_img)
+def get_reshaped_images(X)
+    X_reshaped = []
+    for i,img in enumerate(X):
+        try:
+            img = cv2.resize(img,(200,200))
+            X_reshaped.append(img)
+        except:
+            del Y[i]
+    X_reshaped = np.array(X_reshaped)
+    return X_reshaped
 
+def get_vocab(words)
+    word_to_index = {}
+    index_to_word = {}
+    i = 0
+    for label in words:
+        if label not in word_to_index:
+            word_to_index[label] = i
+            index_to_word[i] = label
+            i += 1
+    return word_to_index, index_to_word
 
+def get_labels(words,word_to_index):
+    Y = [word_to_index[word] for word in words]
+    return Y
 
