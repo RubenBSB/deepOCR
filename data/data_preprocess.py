b58f4d223cf338e488559bd086717f6df7d1a8fe
Ruben Bousbib <rbousbib@gmail.com>
2018-12-17 23:40:19 +0100

    One function to preprocess data.

--- data/data_preprocess.py
+++ data/data_preprocess.py
@@ -1,40 +1,41 @@
 import numpy as np
 import cv2
+import sys
 import glob
 import re
 
-root = "data/words/*/*/*.png"
-filenames = sorted(glob.glob(root))
-
-def get_images():
-    global filenames
+def get_dataset(root, resize = 200, print_logs = True):
+    """
+    Returns X an array including all of the resized images and 'words' the associated string labels.
+    """
+    filenames = glob.glob(root+"/words/*/*/*.png")
     X = []
-    for file in filenames:
-        img = cv2.imread(file,0)
-        X.append(img)
-    return X
-
-def get_words():
     pattern = "[^ ]+$"
-    words =[]
-    with open("data/words.txt", 'rb') as labels:
+    words = []
+    total = len(filenames)
+    increment = int(total/50)
+
+    with open(root+"/words.txt", 'rb') as labels:
        for line in labels.read().splitlines()[18:]:
             match = re.findall(pattern,line.decode())[0]
             words.append(match)
-    return words
 
-def get_reshaped_images(X)
-    X_reshaped = []
-    for i,img in enumerate(X):
-        try:
-            img = cv2.resize(img,(200,200))
-            X_reshaped.append(img)
-        except:
-            del Y[i]
-    X_reshaped = np.array(X_reshaped)
-    return X_reshaped
+    for i,file in enumerate(filenames):
+        img = cv2.imread(file, 0)
+        if img is not None:
+            img = cv2.resize(img, (resize, resize))
+            X.append(img)
+        else:
+            del words[i]
+        if print_logs:
+            sys.stdout.write("\r Data preprocessing : [" + "=" * int((i / increment)) + " " * int((((total - i) / increment))) + "] "
+                             + str(round((i / total)*100,1)) + "%")
+            sys.stdout.flush()
 
-def get_vocab(words)
+    X = np.array(X)
+    return X, words
+
+def get_vocab(words):
     word_to_index = {}
     index_to_word = {}
     i = 0
@@ -44,8 +45,3 @@ def get_vocab(words)
             index_to_word[i] = label
             i += 1
     return word_to_index, index_to_word
-
-def get_labels(words,word_to_index):
-    Y = [word_to_index[word] for word in words]
-    return Y
-
