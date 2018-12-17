18aae808f10d39ad4201f2bef5704a6232c73766
Ruben Bousbib <rbousbib@gmail.com>
2018-12-17 23:43:03 +0100

    Optimizer changed.

--- model/basic_model.py
+++ model/basic_model.py
@@ -1,32 +1,40 @@
 import numpy
+from data.data_preprocess import get_dataset, get_vocab
 from keras.models import Model
 from keras.layers import Dense
 from keras.layers import Input
 from keras.layers import GlobalAveragePooling2D
 from keras.layers import Dropout
 from keras.constraints import maxnorm
-from keras.optimizers import SGD
+from keras.optimizers import Adam
 from keras.layers.convolutional import Conv2D
 from keras.layers.convolutional import MaxPooling2D
 from keras.utils import np_utils, to_categorical
 from keras import backend as K
 
+X,words = get_dataset("../data")
+X = np.reshape()
+
+word_to_index, index_to_word = get_vocab(words)
+num_classes = len(word_to_index)
+Y = [word_to_index[word] for word in words]
+Y = to_categorical(Y,num_classes)
 
 def convModel(num_classes):
-    X0 = Input((1, None, None))
+    X0 = Input((200, 200, 1))
 
-    X = Conv2D(32, (3, 3), input_shape=(1, None, None), activation='relu', padding='same')(X0)
+    X = Conv2D(32, (3, 3), input_shape=(200, 200, 1), activation='relu')(X0)
     X = Dropout(0.2)(X)
-    X = Conv2D(32, (3, 3), activation='relu', padding='same')(X)
+    X = Conv2D(32, (3, 3), activation='relu')(X)
     X = MaxPooling2D(pool_size=(2, 2))(X)
-    X = Conv2D(64, (3, 3), activation='relu', padding='same')(X)
+    X = Conv2D(64, (3, 3), activation='relu')(X)
     x = Dropout(0.2)(X)
-    X = Conv2D(64, (3, 3), activation='relu', padding='same')(X)
+    X = Conv2D(64, (3, 3), activation='relu')(X)
     X = MaxPooling2D(pool_size=(2, 2))(X)
-    X = Conv2D(128, (3, 3), activation='relu', padding='same')(X)
+    X = Conv2D(128, (3, 3), activation='relu')(X)
     X = Dropout(0.2)(X)
-    X = Conv2D(128, (3, 3), activation='relu', padding='same')(X)
-    X = GlobalAveragePooling2D(data_format='channels_first')(X)
+    X = Conv2D(128, (3, 3), activation='relu')(X)
+    X = GlobalAveragePooling2D()(X)
     X = Dropout(0.2)(X)
     X = Dense(1024, activation='relu', kernel_constraint=maxnorm(3))(X)
     X = Dropout(0.2)(X)
@@ -40,9 +48,18 @@ def convModel(num_classes):
 model = convModel(num_classes)
 
 epochs = 25
-lrate = 0.01
-decay = lrate/epochs
-sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
-model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+lr = 0.01
+epsilon = 10**(-8)
+decay = lr/epochs
+opt = Adam(lr=lr, decay=decay)
+model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
 print(model.summary())
+
+model.fit(X,Y)
+
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+model.save_weights("model.h5")
+print("Saved model to disk")
 
