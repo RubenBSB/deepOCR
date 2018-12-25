ec5f500bd1d279ea03270e405667d8a46a002ad2
Ruben Bousbib <rbousbib@gmail.com>
2018-12-25 20:07:37 +0100

    New CNN layers for 'same' padding.

--- model/model.py
+++ model/model.py
@@ -2,25 +2,39 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class Conv2dSame(torch.nn.Module):
+
+    def __init__(self, in_channels, out_channels, kernel_size, bias=True, padding_layer=torch.nn.ZeroPad2d):
+        super().__init__()
+        ka = kernel_size // 2
+        kb = ka - 1 if kernel_size % 2 == 0 else ka
+        self.net = torch.nn.Sequential(
+            padding_layer((ka,kb,ka,kb)),
+            torch.nn.Conv2d(in_channels, out_channels, kernel_size, bias=bias)
+        )
+    def forward(self, x):
+        return self.net(x)
+
 class DeepOCR(nn.Module):
 
     def __init__(self,input_size):
         super(DeepOCR,self).__init__()
+        assert(isinstance(input_size,tuple))
 
         input_h, input_w = input_size
 
-        self.conv1 = nn.Conv2d(1,32,5)
-        self.conv2 = nn.Conv2d(32,64,5)
-        self.conv3 = nn.Conv2d(64,128,3)
-        self.conv4 = nn.Conv2d(128,128,3)
-        self.conv5 = nn.Conv2d(128,256,3)
+        self.conv1 = Conv2dSame(1,32,5)
+        self.conv2 = Conv2dSame(32,64,5)
+        self.conv3 = Conv2dSame(64,128,3)
+        self.conv4 = Conv2dSame(128,128,3)
+        self.conv5 = Conv2dSame(128,256,3)
 
         self.pool1 = nn.MaxPool2d((2,2))
         self.pool2 = nn.MaxPool2d((2,1))
 
-        self.lstm = nn.LSTM(256*input_h/32,256,num_layers=2)
+        self.lstm = nn.LSTM(int(256*input_h/32),256,num_layers=2)
 
-        self.fc = nn.Linear(256,80)
+        self.fc = nn.Linear(256,79)
 
 
     def forward(self,x):
@@ -31,6 +45,6 @@ class DeepOCR(nn.Module):
         x = self.pool2(F.relu(self.conv5(x)))
         x = x.view(x.size(0),x.size(1)*x.size(2),-1)
         x = self.lstm(x)
-        x = F.softmax(self.fc(x))
+        x = F.log_softmax(self.fc(x))
 
         return x
