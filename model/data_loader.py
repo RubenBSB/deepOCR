e621be809e2386e0b073f2f4fb1a87bc347142d4
Ruben Bousbib <rbousbib@gmail.com>
2018-12-25 20:06:04 +0100

    Labels encoded into one hot vectors.

--- model/data_loader.py
+++ model/data_loader.py
@@ -1,5 +1,5 @@
 import cv2
-import pandas as pd
+import numpy as np
 import os
 import re
 import glob
@@ -13,22 +13,34 @@ class IAM_Dataset(Dataset):
         self.filenames = sorted(glob.glob(root_dir+"/words/*/*/*.png"))
         self.labels = []
         self.transform = transform
+        self.char_to_index = {}
+        self.index_to_char = {}
 
         labels_path = os.path.join(root_dir,"words.txt")
         with open(labels_path, 'rb') as f:
+            last_index = 0
             for line in f.read().splitlines()[18:]:
                 match = re.findall("[^ ]+$", line.decode())[0]
+                for char in match:
+                    if char not in self.char_to_index:
+                        self.char_to_index[char] = last_index
+                        self.index_to_char[last_index] = char
+                        last_index += 1
                 self.labels.append(match)
-
+        self.char_to_index['BLANK'] = len(self.char_to_index)
+        self.index_to_char[len(self.char_to_index)] = 'BLANK'
 
     def __len__(self):
         return len(self.filenames)
 
     def __getitem__(self, idx):
         img = cv2.imread(self.filenames[idx],cv2.IMREAD_GRAYSCALE)
-        sample = {'image': img, 'label': self.labels[idx]}
+        label = self.labels[idx]
+        target = [np.eye(len(self.char_to_index))[self.char_to_index[char]] for char in label]
 
-        if self.transform:
+        sample = {'image': img, 'label': label, 'target': target}
+
+        if self.transform and img is not None:
             sample = self.transform(sample)
 
         return sample
